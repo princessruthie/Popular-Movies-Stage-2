8b22132d43992fa529f74994bcf27acbe93bb8e9
princessruthie <missruthie@gmail.com>
2016-07-21 13:09:39 -0400

    Implement parcelable.  (parcelable.com)

--- app/src/main/java/com/ruthiefloats/popularmoviesstage1/model/Movie.java
+++ app/src/main/java/com/ruthiefloats/popularmoviesstage1/model/Movie.java
@@ -1,9 +1,12 @@
 package com.ruthiefloats.popularmoviesstage1.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created on 7/18/16.
  */
-public class Movie {
+public class Movie implements Parcelable {
     private String title;
     private String release_date;
     private String poster_path;
@@ -66,4 +69,39 @@ public class Movie {
     public void setOverview(String overview) {
         this.overview = overview;
     }
+
+    protected Movie(Parcel in) {
+        title = in.readString();
+        release_date = in.readString();
+        poster_path = in.readString();
+        vote_average = in.readDouble();
+        overview = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(release_date);
+        dest.writeString(poster_path);
+        dest.writeDouble(vote_average);
+        dest.writeString(overview);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 }
