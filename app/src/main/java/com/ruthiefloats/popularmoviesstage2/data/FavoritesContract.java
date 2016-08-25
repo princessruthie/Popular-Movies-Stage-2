0eb553ab638fdee0711a9a628acc8f68fc76fdea
princessruthie <missruthie@gmail.com>
2016-08-25 16:10:45 -0400

    tidy: Fix typo and formatting in FavoritesContract

--- app/src/main/java/com/ruthiefloats/popularmoviesstage2/data/FavoritesContract.java
+++ app/src/main/java/com/ruthiefloats/popularmoviesstage2/data/FavoritesContract.java
@@ -10,17 +10,15 @@ import android.provider.BaseColumns;
 public class FavoritesContract {
 
     public static final String CONTENT_AUTHORITY = "com.ruthiefloats.popular-movies-stage-2";
-
-    private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
-
     public static final String PATH_FAVORITES = "favorites";
+    private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     public static final class Favorites implements BaseColumns {
 
         public static final Uri CONTENT_URI =
                 BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVORITES).build();
         public static final String CONTENT_TYPE =
-                "vnd.adroid.cursor.dir/" + CONTENT_URI + "/" + PATH_FAVORITES;
+                "vnd.android.cursor.dir/" + CONTENT_URI + "/" + PATH_FAVORITES;
         // Table name
         public static final String TABLE_NAME = "favorites";
 
@@ -30,7 +28,7 @@ public class FavoritesContract {
         public static final String COLUMN_RELEASE_DATE = "release_date";
         public static final String COLUMN_API_ID = "api_id";
 
-        public static Uri buildMovieUri(long id){
+        public static Uri buildMovieUri(long id) {
             return ContentUris.withAppendedId(CONTENT_URI, id);
         }
     }
